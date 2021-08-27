db8961b98d6ede0735ffbb30420df85057bad56f
hrishi-19 <hrishakamath@gmail.com>
2021-08-27 18:14:45 +0530

    adding functionality

--- SPS/js/index.js
+++ SPS/js/index.js
@@ -1,5 +1,22 @@
+const HandOptions={
+    "rock":"/SPS/assets/Rock.png",
+    "paper":"/SPS/assets/Paper.png",
+    "scissor":"/SPS/assets/Scissors.png"
+}
 
 const PickUserHand=(hand)=>{
     let handSelected=document.querySelector('.game');
+    let AfterSelected=document.querySelector('.result');
     handSelected.style.display="none";
+    AfterSelected.style.display='flex';
+
+    //set userPickedHand
+    document.getElementById("PickedHand").src=HandOptions[hand];
+    
+}
+const restart=()=>{
+    let handSelected=document.querySelector('.game');
+    let AfterSelected=document.querySelector('.result');
+    handSelected.style.display="flex";
+    AfterSelected.style.display='none';
 }
